d0ac3ef25471b9830fdd03c211120da14133f94c
danielkalen <d@danielkalen.com>
2017-08-02 12:06:31 -0400

    add "-mod" suffix to plugin version

--- acf.php
+++ acf.php
@@ -3,7 +3,7 @@
 Plugin Name: Advanced Custom Fields PRO
 Plugin URI: https://www.advancedcustomfields.com/
 Description: Customise WordPress with powerful, professional and intuitive fields.
-Version: 5.6.0
+Version: 5.6.0-mod
 Author: Elliot Condon
 Author URI: http://www.elliotcondon.com/
 Copyright: Elliot Condon
